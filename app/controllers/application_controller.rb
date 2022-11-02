e15e80fbeded8504f97ff5bf799f8d6dfd70cde5
worashf <uppert83@gmail.com>
2022-11-02 21:11:13 +0300

    Allow devise sign up paramS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::API
+  respond_to :json
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name,:username, :email, :password) }
+  end
+
+
   rescue_from ActiveRecord::RecordNotFound do
-    render json: { error: 'Resource not found!' }, status: :not_found
+  render json: { error: 'Resource not found!' }, status: :not_found
   end
 
   rescue_from ActionController::RoutingError do
